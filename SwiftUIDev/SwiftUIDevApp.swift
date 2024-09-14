091afd432a62ed1e02c377f2269a8bf13563e1bb
mamong <1054209736@qq.com>
2024-09-14 14:49:54 +0800

    Initial Commit

---
+++ SwiftUIDev/SwiftUIDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDevApp.swift
+//  SwiftUIDev
+//
+//  Created by tryao on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
